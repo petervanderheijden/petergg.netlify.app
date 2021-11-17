56a5dc41c0dad06f34b3ced240d83f9eb290b98b
PeterGG <enzopeter02@gmail.com>
2021-11-17 17:36:04 +0100

    v2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,12 @@
 function myFunction() {
-    alert("Hello, world!");
+    var para = document.getElementsByClassName("navbar__links");
+
+    if(para.classList.contains("navbar__links")) {
+        para.classList.remove("navbar__links");
+        para.classList.add("navbar__links_open");
+    }
+    else {
+        para.classList.remove("navbar__links_open");
+        para.classList.add("navbar__links");
+    }
 }
